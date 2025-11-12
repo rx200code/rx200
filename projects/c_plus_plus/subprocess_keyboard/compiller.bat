1dcc14764be4362c0156025066df9755ffcd7ca6
rx200code <priroda5000@gmail.com>
2025-11-13 02:37:35 +0700

    Add files via upload

---
+++ projects/c_plus_plus/subprocess_keyboard/compiller.bat
@@ -0,0 +1,7 @@
+cd R:\pro\c_plus_plus\pro\x86_64-8.1.0-release-posix-sjlj-rt_v6-rev0\mingw64\bin
+gcc.exe R:\pro\c_plus_plus\subprocess_keyboard\test.cpp -Wall -lstdc++ -o R:\pro\c_plus_plus\subprocess_keyboard\test.exe
+pause
+cd R:\pro\c_plus_plus\subprocess_keyboard
+test.exe
+echo %ERRORLEVEL%
+pause
---
+++ projects/c_plus_plus/subprocess_keyboard/run_test.bat
@@ -0,0 +1,5 @@
+@echo off
+rem echo 3 4 |
+test
+echo Программа завершена с кодом: %ERRORLEVEL%
+pause
