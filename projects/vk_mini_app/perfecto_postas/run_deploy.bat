7ea46868f045088dea6725a45741e3f20bdd2bee
rx200code <priroda5000@gmail.com>
2025-11-13 03:52:24 +0700

    Add files via upload

---
+++ projects/vk_mini_app/perfecto_postas/run_deploy.bat
@@ -0,0 +1,7 @@
+pause
+pause
+set source_run=R:\pro\node_js\node-v20.16.0-win-x64
+set PATH=%source_run%;%path%
+
+npm run deploy
+pause
---
+++ projects/vk_mini_app/perfecto_postas/test_run_host.bat
@@ -0,0 +1,5 @@
+set source_run=R:\pro\node_js\node-v20.16.0-win-x64
+set PATH=%source_run%;%path%
+
+npm run start -- --host
+pause
