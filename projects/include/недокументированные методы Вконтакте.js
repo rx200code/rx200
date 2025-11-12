0a10746454c73423a81ac4732a8e088cadf40ce2
rx200code <priroda5000@gmail.com>
2025-11-13 02:52:42 +0700

    Add files via upload

---
+++ projects/include/недокументированные методы Вконтакте.js
@@ -0,0 +1,19 @@
+//############ Удаление из подписчиков #########################
+https://vk.com/friends?section=requests
+// для удаления из подписчиков https://vk.com/id316141058
+
+// вызывается метод 
+https://api.vk.com/method/friends.deleteSubscriber?v=5.245&client_id=6287487
+
+// запрос
+{
+	"subscriber_id": "316141058",
+	"ref": "friends_requests_all_requests_list",
+	"access_token": "vk1..."
+}
+
+// ответ
+{
+	"response": 1
+}
+//##############################################################
